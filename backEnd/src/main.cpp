9a638e088e02fa3a1e58217badadbdd2e7fdae81
mark <1464089197@qq.com>
2024-11-03 21:44:11 +0800

    程序运行时，不展示控制台

--- backEnd/src/main.cpp
+++ backEnd/src/main.cpp
@@ -1,22 +1,32 @@
-// #define CPPHTTPLIB_OPENSSL_SUPPORT
 
 #include "httplib.h"
+
+#if defined(_WIN32) || defined(_WIN64)
 #include "rc.h"
+#endif
 
-int main() {
-
-    load_rc();
+void do_main()
+{
 
     // HTTP
     httplib::Server svr;
 
-    // HTTPS
-    // httplib::SSLServer svr;
-
     svr.Get("/hi",
-            [](const httplib::Request &, httplib::Response &res) { res.set_content("Hello World!", "text/plain"); });
+            [](const httplib::Request &, httplib::Response &res)
+            { res.set_content("Hello World!", "text/plain"); });
 
     svr.listen("0.0.0.0", 8080);
-    return 0;
 }
 
+#if defined(_WIN32) || defined(_WIN64)
+// Windows GUI程序入口
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
+    // 为exe程序添加图标
+    load_rc();
+
+    do_main();
+
+    return 0;
+}
+#endif
